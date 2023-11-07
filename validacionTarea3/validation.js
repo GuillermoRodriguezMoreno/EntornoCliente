229da8fca0ff96e3b45449e8d56df6213689c523
Guillermo <guille@example.com>
2023-11-07 10:53:15 +0100

    s

--- validacionTarea3/validation.js
+++ validacionTarea3/validation.js
@@ -10,21 +10,25 @@ function validaEmail(){
 
     let test = true;
 
+    // No tiene 10 caracteres
     if(email.validity.tooShort){
 
         email.setCustomValidity("El email debe tener al menos 10 caracteres")
         test = false;
         
+    // No es formato email
     }else if(email.validity.typeMismatch){
 
         email.setCustomValidity("El email debe tener al menos una letra, @, una letra, un punto y un dominio de minimo dos letras. Ejemplo: a@a.uk")
         test = false;
 
+    // Esta vacio
     }else if(email.validity.valueMissing){
 
         email.setCustomValidity("El email es obligatorio")
         test = false;
 
+    // No cumple la expresion regular
     }else if(email.validity.patternMismatch){
 
         email.setCustomValidity("El email debe tener al menos una letra, @, una letra, un punto y un dominio de minimo dos letras. Ejemplo: a@a.uk")
