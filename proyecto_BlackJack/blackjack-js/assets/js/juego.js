376924c1b7f03e89c887f3abb4c4eccd81108673
Guillermo <guille@example.com>
2023-11-14 11:03:07 +0100

    init

--- proyecto_BlackJack/blackjack-js/assets/js/juego.js
+++ proyecto_BlackJack/blackjack-js/assets/js/juego.js
@@ -9,10 +9,65 @@
 let baraja = crearBaraja();
 let puntosJugador = 0;
 let puntosComputadora = 0;
+let turno = "jugador";
+
+// Variable HTML
+const botonPedirCarta = document.getElementById("btnPedir");
+const botonDetener = document.getElementById("btnDetener");
+const botonNuevo = document.getElementById("btnNuevo");
+const cartasJugador = document.getElementById("jugador-cartas");
+const cartasComputadora = document.getElementById("computadora-cartas");
+const contPuntosJugador = document.getElementById("puntosJugador");
+const contPuntosComputadora = document.getElementById("puntosComputadora");
+
+// Main
+
+main();
+
+// Añadir eventos
+
+botonPedirCarta.addEventListener("click",funcionalidadPedirCarta);
+botonDetener.addEventListener("click", funcionalidadDetener)
+botonNuevo.addEventListener("click", reset);
+
+
+// Funciones
+
+function funcionalidadPedirCarta(){
+
+    let carta = sacarCarta();
+
+    insertarImgCarta(carta);
+
+    let puntos = sumarPuntos(carta);
+
+    if(comprobarPuntos(puntos)){
+
+        botonPedirCarta.setAttribute("disabled", true);
+    }
+
+}
+
+function funcionalidadDetener(){
+
+    botonPedirCarta.disabled = false;
+
+    if(turno == "jugador"){
+
+        turno = "computadora";
+
+    } else{
+
+        botonPedirCarta.setAttribute("disabled", true);
+        alert(comprobarGanador());
+    }
+}
 
 function main(){
 
+    baraja = crearBaraja();
 
+    barajar();
 
 }
 
@@ -87,7 +142,7 @@ function obtenerImagen(carta){
 // Para tener nombre mas descriptivo
 function barajar(){
 
-    _.shuffle(baraja);
+    baraja = _.shuffle(baraja);
 }
 
 function sacarCarta(){
@@ -96,9 +151,89 @@ function sacarCarta(){
 }
 
 // Insertar imagenCarta
-function insertarImgCarta(carta, player){
+function insertarImgCarta(carta){
 
+    let path = "../cartas/";
+    let img = document.createElement("img");
+    img.src = path + obtenerImagen(carta);
+    img.className = "carta";
+
+    if(turno == "jugador"){
+
+        cartasJugador.append(img);
+
+    } else{
+
+        cartasComputadora.append(img);
+    }
 
 }
 
+function sumarPuntos(carta){
+
+    if(turno == "jugador"){
+
+        puntosJugador = puntosJugador + carta[1];
+        contPuntosJugador.innerText = puntosJugador;
+        return puntosJugador;
+
+    } else{
+
+        puntosComputadora = puntosComputadora + carta[1];
+        contPuntosComputadora.innerText = puntosComputadora;
+        return puntosComputadora;
+    }
+}
+
+function comprobarPuntos(puntos){
+
+    let control = false;
+
+    if(puntos > 21){
+
+        control = true;
+    }
+
+    return control;
+}
+
+function reset(){
+
+    puntosComputadora = 0;
+    puntosJugador = 0;
+    contPuntosJugador.innerText = puntosJugador;
+    contPuntosComputadora.innerText = puntosComputadora;
+    borrarCartas();
+}
+
+function comprobarGanador(){
+
+    let ganador = "";
+
+    if(puntosJugador > puntosComputadora){
+
+        ganador = "jugador";
+
+    } else{
+
+        ganador = "computadora";
+    }
+
+    return ganador;
+}
+
+function borrarCartas(){
+
+    while (cartasJugador.firstChild) {
+
+        cartasJugador.removeChild(cartasJugador.firstChild);
+
+    }
+
+    while (cartasComputadora.firstChild) {
+
+        cartasComputadora.removeChild(cartasComputadora.firstChild);
+
+    }
+}
 
