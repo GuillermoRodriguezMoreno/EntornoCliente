b53c57f16b97bf8b48212607a1e269a8cb18a0f9
Guillermo <guille@example.com>
2023-12-04 12:24:25 +0100

    init

--- tarea-js-promises/main.js
+++ tarea-js-promises/main.js
@@ -5,6 +5,8 @@ const baseURL = "http://localhost:3000/";
 const contenedorListadoPelicula = document.getElementById("listado");
 const infoDirector = document.getElementById("director");
 const infoClasificacion = document.getElementById("clasificacion");
+const infoValoracion = document.getElementById("estrellas");
+const infoCartel = document.querySelector("#cartel img");
 
 // Variables globales
 let listaPeliculas = [];
@@ -22,7 +24,8 @@ function insertarListado(listaPeliculas){
     
         li = document.createElement("li")
         li.innerText = pelicula.nombre
-        li.addEventListener("click", mostrarInformacion)
+    
+        li.addEventListener("click", () => {mostrarInformacion(pelicula.id)})
         contenedorListadoPelicula.append(li);
     }
 }
@@ -30,6 +33,7 @@ function insertarListado(listaPeliculas){
 function mostrarListaPeliculas(){
 
     fetch(baseURL + "peliculas/").then( resp => {
+
         if (resp.status == 200) {
     
             resp.json().then(data => {
@@ -42,12 +46,81 @@ function mostrarListaPeliculas(){
     
         } else {
     
-            listaPeliculas=null
             console.log("El recurso no existe");
         }
+        
     }).catch ( error => console.log("fetch error:" + error));
 }
 
-function mostrarInformacion(){
+function mostrarInformacion(id){
     
+    fetch(baseURL + "pelicula/" + id.toString()).then( resp => {
+
+        let pelicula = null
+
+        if (resp.status == 200) {
+    
+            resp.json().then(data => {
+    
+                pelicula = data;
+                insertarDatos(pelicula);
+                console.log("fetch leído");
+            })
+
+        } else {
+
+            console.log("El recurso no existe");
+        }
+
+    }).catch ( error => console.log("fetch error:" + error));
+}
+
+function insertarDatos(pelicula){
+
+    let clasificacion = null;
+
+    infoDirector.innerText = pelicula.director;
+    clasificacion = buscarClasificacion(pelicula.clasificacion);
+    infoClasificacion.innerText = clasificacion.nombre;
+    pintarClasificacion(pelicula);
+    infoCartel.setAttribute("src", "assets/imgs/" + pelicula.cartel);
+
+}
+
+function buscarClasificacion(idClasificacion){
+
+    let clasificacion = null;
+    let ruta = baseURL + "clasificaciones/" + idClasificacion // traza depuracion
+
+    fetch(ruta).then( resp => {
+
+        if (resp.status == 200) {
+    
+            resp.json().then(data => {
+    
+                clasificacion = data
+                console.log("fetch leído");
+                return clasificacion;
+            })
+    
+        } else {
+    
+            console.log("El recurso no existe");
+        }
+
+        return clasificacion;
+        
+    }).catch ( error => console.log("fetch error:" + error));
+}
+
+function pintarClasificacion(pelicula){
+
+    let estrellasTotales = pelicula.valoracion;
+
+    for (let i = 0; i < estrellasTotales; i++) {
+
+        let iconoEstrella = document.createElement("i");
+        iconoEstrella.className = "fa fa-star";
+        infoValoracion.append(iconoEstrella);
+    }
 }
